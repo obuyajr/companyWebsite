0fa4dbd14c9143f593a5b618db9ed4fb54711e71
obuyajr <felix37brock@gmail.com>
2024-03-21 14:16:10 +0300

    FFRM PAYMENT

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ let Fabars =  document.querySelector('.fa-bars');
 
 Fabars.onclick =  () =>{
     NavBar.classList.toggle("active")
-}
+};
+
+
+var swiper = new Swiper(".home-slide", {});
+// var swiper = new Swiper(".home-slide", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
